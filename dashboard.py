84074f63be9f9370eff77fa48ad15738e03704ac
roman200110 <romandilif2001@gmail.com>
2024-05-14 17:24:16 +0400

    added some new features

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Load the car data
-@st.cache
+@st.cache_data
 def load_data():
     return pd.read_csv('data/cleaned_car_data_year.csv')
 
@@ -73,18 +74,18 @@ st.plotly_chart(fig_scatter)
 max_power_cars = filtered_data.loc[filtered_data.groupby('Make')['Max Power'].idxmax()]
 # Bar chart of Max Power by Make
 st.header("Bar chart of Max Power by Make")
-fig_max_power = px.bar(max_power_cars, x='Make', y='Max Power', hover_data=['Model'])
+fig_max_power = px.bar(max_power_cars, x='Make', y='Max Power', color='Model', hover_data=['Model'])
 st.plotly_chart(fig_max_power)
 
-# Bar chart of Make
-st.header("Bar chart of Make")
-make_counts = filtered_data['Make'].value_counts()
-# Create a DataFrame from the value counts
-make_counts_df = pd.DataFrame({'Make': make_counts.index, 'Count': make_counts.values})
+# Create a DataFrame that contains the count of each 'Make'
+make_counts = filtered_data['Make'].value_counts().reset_index()
+make_counts.columns = ['Make', 'Count']
 
 # Create the bar chart
-fig_bar = px.bar(make_counts_df, x='Make', y='Count')
-st.plotly_chart(fig_bar)
+fig_make_count = px.bar(make_counts, x='Make', y='Count', color='Make')
+
+# Display the figure in Streamlit
+st.plotly_chart(fig_make_count)
 
 # Pie chart of Fuel Type
 st.header("Pie chart of Fuel Type")
@@ -95,3 +96,40 @@ st.plotly_chart(fig_pie)
 st.header("Box plot of Price by Transmission type")
 fig_box = px.box(filtered_data, x='Transmission', y='Price')
 st.plotly_chart(fig_box)
+
+
+# Initialize an empty figure
+fig = go.Figure()
+
+# Iterate over the unique car makes
+for make in filtered_data['Make'].unique():
+    # Filter the data for the current make
+    make_data = filtered_data[filtered_data['Make'] == make]
+
+    # Calculate the mean price for each year for the current make
+    mean_prices = make_data.groupby('Year')['Price'].mean().reset_index()
+
+    # Create a scatter plot for the mean prices
+    mean_price_trace = go.Scatter(
+        x=mean_prices['Year'],
+        y=mean_prices['Price'],
+        mode='lines+markers',
+        name=f'Mean Price of {make}'
+    )
+
+    # Add the scatter plot to the figure
+    fig.add_trace(mean_price_trace)
+
+# Set the y-axis to a log scale
+fig.update_yaxes(type="log")
+
+# Add title and labels
+fig.update_layout(
+    title="Price vs Age with Mean Price Trajectory for Each Make",
+    xaxis_title="Age",
+    yaxis_title="Price",
+    hovermode="closest"
+)
+
+# Display the figure in Streamlit
+st.plotly_chart(fig)
